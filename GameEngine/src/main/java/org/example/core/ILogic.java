a9546f41fdcf263bc03e45bedee3d366e99a8378
7aidary <107144969+7aidary@users.noreply.github.com>
2025-05-08 23:22:17 +0300

    Delete GameEngine directory

--- GameEngine/src/main/java/org/example/core/ILogic.java
+++
@@ -1,9 +0,0 @@
-package org.example.core;
-
-public interface ILogic {
-    void init() throws Exception;
-    void input();
-    void update(float intervals,MouseInput mouseInput);
-    void render();
-    void cleanup();
-}
